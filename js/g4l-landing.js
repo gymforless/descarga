07fc8bbc13e10205ab4c35661716994c08bd0cf9
Guillermo Ceballos <willeeh@gmail.com>
2013-12-28 19:19:23 +0000

    First approach to get GoogleMaps

--- js/g4l-landing.js
+++ js/g4l-landing.js
@@ -1,3 +1,5 @@
+var map;
+
 (function(){
 	var ua = navigator.userAgent,
 		isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
@@ -19,7 +21,64 @@
         send();
     };
 
+    function initializeMap() {
+        //Spain locations
+        /*var lat = 40.178873;
+        var lng = -3.361816;*/
+        //Barcelona location
+        var lat = 41.396385;
+        var lng = 2.174606;
+        var mapOptions = {
+          center: new google.maps.LatLng(lat,lng),
+          zoom: 12
+        };
+        map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+    }
+    google.maps.event.addDomListener(window, 'load', initializeMap);
+
+
+    //Barcelona by default so far
+    var latitude=41.396385;
+    var longitude=2.174606;
+
+    var url = 'http://gymforless.herokuapp.com/api/gyms?longitude='+longitude+'&latitude='+latitude;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        success: annotations
+    });
+
 })();
+
+function annotations(data, textStatus, jqXHR)
+{
+    if (textStatus == "success")
+    {
+        data.forEach(function(gym) {
+            if (gym.location.latitude != null && gym.location.longitude != null) {
+                var loc = new google.maps.LatLng(gym.location.latitude,gym.location.longitude);
+                var image = 'img/annotation.png';
+                var marker = new google.maps.Marker({
+                    position: loc,
+                    map: map,
+                    icon: image,
+                    title: gym.name
+                });
+
+                var content = "<div class='callout'>"+gym.name+"</div>"
+
+                var infowindow = new google.maps.InfoWindow({
+                      content: content
+                  });
+
+                google.maps.event.addListener(marker, 'click', function() {
+                    infowindow.open(map,marker);
+                });
+            }
+        });
+    }
+}
 
 function send()
 {
